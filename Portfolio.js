ff040594740ed721da44dad027f4895a5b1761f9
Ahmadi-Sajjad <ahmadi.sajjad.2005@gmail.com>
2026-02-05 10:44:10 +0330

    Refactor:structure and nameing changed, cdn removed

--- Portfolio.js
+++ Portfolio.js
@@ -224,3 +224,18 @@ document.addEventListener('click', (e) => {
         closeAllSkills();
     }
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
